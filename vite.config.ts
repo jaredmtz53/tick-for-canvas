a3ae4ce3077fab52da3ed62690b2c9453454323b
jaredmtz53 <jaredmtz53@icloud.com>
2025-05-01 10:15:14 -0400

    basic strucutre of a chrome extension

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      input: {
+        popup: './popup/index.html',
+      },
+      output: {
+        entryFileNames: "assets/[name].js",
+      }
+
+    }
+  }
+})
